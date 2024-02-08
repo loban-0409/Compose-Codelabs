253b02397009167e0a714574937c98f5ca170dec
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2024-02-08 21:18:33 +0900

    feat: UnscrambleApp 구현

--- Unscramble-Starter/app/src/main/java/com/example/unscramble/MainActivity.kt
+++ Unscramble-Starter/app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -22,7 +22,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.unscramble.ui.GameScreen
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
@@ -39,5 +41,14 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun UnscrambleApp() {
+    UnscrambleTheme {
+        GameScreen()
     }
 }
